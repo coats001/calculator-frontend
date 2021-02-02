94f08f1fd782975f3ca1071ab6792667a709a595
coats001 <coats01@hotmail.com>
2021-02-02 02:18:29 +0100

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
+import {Calculator} from './model/calculator';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +10,7 @@ import {HttpClient} from '@angular/common/http';
 export class AppComponent {
   title = 'calc-frontend';
   url = 'http://localhost:8080/';
+  calculatorModel = new Calculator(1, 1, '-');
 
   submit(calculator: NgForm) {
     console.log('Form submitted', calculator);
---
+++ src/app/model/calculator.ts
@@ -0,0 +1,8 @@
+export class Calculator {
+  constructor(
+    public a: number,
+    public b: number,
+    public operator: string
+  ) {
+  }
+}
